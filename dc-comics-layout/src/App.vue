7a18257557c48051dfb73b4fd15697f2cb2467ae
Rosario Nici <rosarionici95@gmail.com>
2022-09-25 20:40:56 +0200

    Setup app

---
+++ dc-comics-layout/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <HeaderComponent/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    
+}
+}
+</script>
+
+<style lang="scss">
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
