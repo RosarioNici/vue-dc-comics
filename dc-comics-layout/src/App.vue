f02663b9134ea303fda0efeafa60b8e170473e9c
Rosario Nici <rosarionici95@gmail.com>
2022-09-25 21:37:02 +0200

    progres to layaout frist part of exercise

--- dc-comics-layout/src/App.vue
+++ dc-comics-layout/src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
     <HeaderComponent/>
+    <MainComponent/>
+    <FooterComponent/>
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue'
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
 
 
 export default {
   name: 'App',
   components: {
     HeaderComponent,
-    
+    MainComponent,
+    FooterComponent
 }
 }
 </script>
 
 <style lang="scss">
+  $zero-auto: 0 auto;
 *{
   box-sizing: border-box;
   margin: 0;
---
+++ dc-comics-layout/src/components/MainComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <main> <section class="container-main">
+<div> <h1>--> content Goes Here >-- </h1> </div>
+  </section></main>
+ 
+</template>
+
+<script>
+export default {
+name: 'MainComponent'
+}
+</script>
+
+<style lang="scss">
+$zero-auto: 0 auto;
+    main{
+        width: 100%;
+        height: 30%;
+        color: white;
+        background-color: black;
+        margin: $zero-auto;
+    }
+    .container-main{
+        margin: $zero-auto;
+        max-width: 1200px;
+    }
+
+</style>
